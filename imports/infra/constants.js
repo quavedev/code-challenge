427aee21b5110fd8034509e592d735cc33bdbf7c
filipenevola <filipenevola@gmail.com>
2020-12-24 15:38:09 -0400

    Moves files to imports folder

---
+++ imports/infra/constants.js
@@ -0,0 +1,3 @@
+export const TEXTS = {
+  HOME_TITLE: 'Event Check-in',
+};
---
+++ imports/ui/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TEXTS } from '../infra/constants';
+
+export const App = () => (
+  <div>
+    <h1>{TEXTS.HOME_TITLE}</h1>
+  </div>
+);
