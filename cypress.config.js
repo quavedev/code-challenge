634894819fc6a646665924df06a879404c1b3f1c
filipenevola <filipe@quave.com.br>
2023-01-22 09:47:18 -0400

    Fixes styles in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // eslint-disable-next-line global-require
+      return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:3020',
   },
-})
+});
