1d22864e30e9ddd7f1b5a0528ce6880c921f2f2d
VashRogers <rogerssj@gmail.com>
2024-08-08 15:28:55 -0400

    Visualizing people collection

--- ui/src/components/EventSelector/index.jsx
+++ ui/src/components/EventSelector/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-export function EventSelector({ communities }) {
+export function EventSelector({ communities, setSelectedEvent }) {
+  const handleChangeSelect = (e) => {
+    setSelectedEvent(e.target.value);
+  };
+
   return (
     <select
+      onChange={handleChangeSelect}
       id="communities"
       defaultValue=""
       className="block w-1/3 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
@@ -11,7 +16,7 @@ export function EventSelector({ communities }) {
         Select an event
       </option>
       {communities.map((community) => (
-        <option key={community.id} value={community.id}>
+        <option key={community._id} value={community._id}>
           {community.name}
         </option>
       ))}
--- ui/src/home/index.jsx
+++ ui/src/home/index.jsx
@@ -1,10 +1,16 @@
 import { useTracker } from 'meteor/react-meteor-data';
-import React from 'react';
+import React, { useState } from 'react';
 import { Communities } from '../../../communities/communities';
 import { EventSelector } from '../components/EventSelector/index.jsx';
+import { People } from '../../../people/people.js';
 
 export function Home() {
+  const [selectedEvent, setSelectedEvent] = useState("");
+  
   const communities = useTracker(() => Communities.find({}).fetch());
+  const people = useTracker(() => People.find({}).fetch());
+
+  // console.log("people: ", people);
 
   return (
     <div className="flex w-3/4 flex-col rounded-md bg-stone-200 shadow-inner">
@@ -12,9 +18,9 @@ export function Home() {
         <h1 className="text-center text-lg font-bold">Event Check-in</h1>
       </div>
 
-      <div className="flex flex-1">
+      <div className="flex flex-1 flex-col">
         
-        <EventSelector communities={communities} />
+        <EventSelector communities={communities} setSelectedEvent={setSelectedEvent} />
       
       </div>
     </div>
